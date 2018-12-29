3fb8934eff2c840c471fc9d5ce3260ba5346cebd
dragonetail <dragonetail@gmail.com>
2018-12-29 14:45:39 +0800

    normal update

---
+++ src/main/kotlin/com/example/ImageSyncSampleBackend/ImageSyncSampleBackendApplication.kt
@@ -0,0 +1,12 @@
+package com.example.ImageSyncSampleBackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImageSyncSampleBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<ImageSyncSampleBackendApplication>(*args)
+}
+
