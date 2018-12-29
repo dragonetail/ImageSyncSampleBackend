96157e2918d8515df357f2695fa9648a66b557f2
dragonetail <dragonetail@gmail.com>
2018-12-29 15:54:29 +0800

    change back to Java

--- src/main/java/com/example/ImageSyncSampleBackend/ImageSyncSampleBackendApplication.java
+++ src/main/java/com/example/ImageSyncSampleBackend/ImageSyncSampleBackendApplication.java
@@ -1,14 +1,27 @@
 package com.example.ImageSyncSampleBackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.example.ImageSyncSampleBackend.storage.StorageProperties;
+import com.example.ImageSyncSampleBackend.storage.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class ImageSyncSampleBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ImageSyncSampleBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ImageSyncSampleBackendApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 }
-
